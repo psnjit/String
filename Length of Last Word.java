e93403cf2f477a41784bfc4db1cdd2d6f9926633
Prasenjit Saha <prasenjitsaha49@gmail.com>
2020-09-06 23:36:07 +0530

    Create Length of Last Word.java

---
+++ Length of Last Word.java
@@ -0,0 +1,19 @@
+//Length of the last word present in the string
+
+public class Solution {
+    // DO NOT MODIFY THE LIST. IT IS READ ONLY
+    public int lengthOfLastWord(final String A) 
+    {
+        int lastWordStartIndex=0, lastWordEndIndex=-1;
+        for(int i=0; i<A.length(); i++)
+        {
+            if(A.charAt(i)==' ' && (i+1)<A.length() && Character.isLetterOrDigit(A.charAt(i+1)))
+                lastWordStartIndex=i+1;
+                
+            if(Character.isLetterOrDigit(A.charAt(i)))
+                lastWordEndIndex=i;
+        }
+        //String st= A.substring(lastWordIndex, A.length());
+        return lastWordEndIndex-lastWordStartIndex+1;
+    }
+}
